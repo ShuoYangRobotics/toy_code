7e07d78e56246d1155981295399c0858ff35d5c2
paulyang <paulyang1990@gmail.com>
2016-10-26 13:31:31 +0800

    test urdf joint publisher

--- toy_ros_space/src/rc_sim_oub/scripts/rc.py
+++ toy_ros_space/src/rc_sim_oub/scripts/rc.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
+from sensor_msgs.msg import JointState
 
 import pyxhook
 
@@ -85,11 +86,22 @@ def rc_talker():
     hookman.start()
 
     pub = rospy.Publisher('rc_sender', String, queue_size=10)
+    # 2016-10-26 test urdf joint control
+    # controls a file in ~/toy_code/toy_ros_space/src/rc_sim_oub/test.launch
+    #roslaunch test.launch model:='$(find urdf_tutorial)/urdf/07-physics.urdf' gui:=True
+    pub_2 = rospy.Publisher('test_joint', JointState, queue_size=10)
     rospy.init_node('rc_talker')
     rate = rospy.Rate(50)
     while not rospy.is_shutdown():
         #hello_str = "hello world %s" % rospy.get_time()
         pub.publish(''.join(rc_keys))
+
+        # 2016-10-26 test urdf joint control
+        msg = JointState()
+        msg.header.stamp = rospy.Time.now()
+        msg.name = ['gripper_extension']
+        msg.position = [-0.12*int(rc_keys[0])]
+        pub_2.publish(msg)
         #print rc_keys
         rate.sleep()
         if running == False:
