8c9ea2a8db59b5d0aed7d73cb2bd435c6d699abd
Paul <shuo.yang@dji.com>
2017-01-14 10:47:24 +0800

    add a quadrupe robot simulation

---
+++ hopper/init_quad.m
@@ -0,0 +1,27 @@
+%parameters for ground contract
+ground.stiff = 5e4;
+ground.damp = 1000;
+
+%body size
+body.x_length = 0.6;
+body.y_length = 0.3;
+body.z_length = 0.15;
+body.shoulder_size = 0.07;
+body.upper_length = 0.2;
+body.lower_length = 0.2;
+body.foot_radius = 0.035;
+body.shoulder_distance = 0.2;
+
+% parameters for leg control 
+ctrl.k_p = 150;
+ctrl.k_d = 2.8;
+
+% parameters for high level plan
+planner.touch_down_height = 0.239; % from foot center to ground
+planner.stand_height = 0.58; % 0.2*cos(30/180*pi)*2+0.2345 = 0.5809 
+                             % due to joint torque, the exact height is
+                             % 0.5796
+planner.stand_s = 0;
+planner.stand_u = 30*pi/180;
+planner.stand_k = -60*pi/180;
+
