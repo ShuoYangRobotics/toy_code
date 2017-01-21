4c153790cb5a75212f98ee46e020f90656ebe180
Paul <shuo.yang@dji.com>
2017-01-21 21:39:56 +0800

    add a jumper version simple to test jump

--- hopper/init_quad.m
+++ hopper/init_quad.m
@@ -18,9 +18,10 @@ ctrl.k_d = 2.8;
 
 % parameters for high level plan
 planner.touch_down_height = 0.239; % from foot center to ground
-planner.stand_height = 0.58; % 0.2*cos(30/180*pi)*2+0.2345 = 0.5809 
+planner.stand_height = 0.55; % 0.2*cos(30/180*pi)*2+0.2345 = 0.5809 
                              % due to joint torque, the exact height is
                              % 0.5796
+planner.flight_height = 0.35;% when leg is flying in the air
 planner.stand_s = 0;
 planner.stand_u = 30*pi/180;
 planner.stand_k = -60*pi/180;
